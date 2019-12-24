871a71858f018c4b6488969d29244b9ad4f76673
lin <451846939@qq.com>
2019-12-24 23:17:39 +0800

    commit

---
+++ src/main/java/leetcode107/MainClass.java
@@ -0,0 +1,157 @@
+package leetcode107;
+
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其自底向上的层次遍历为：
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        Integer count=0;
+        dfs(root,count,res);
+        Collections.reverse(res);
+        return res;
+    }
+
+    private void dfs(TreeNode root, Integer count, List<List<Integer>> res) {
+        if (root==null){
+            return;
+        }
+        List<Integer> integers = new ArrayList<>();
+        if (res.size()-1>=count){
+            integers= res.get(count);
+        }else {
+            res.add(count,integers);
+        }
+        if (root.left!=null){
+            dfs(root.left,count+1,res);
+        }
+        if (root.right!=null){
+            dfs(root.right,count+1,res);
+        }
+
+
+        integers.add(root.val);
+    }
+}
+
+public class MainClass {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+    
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+    
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+    
+            if (index == parts.length) {
+                break;
+            }
+    
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+    
+            if (index == parts.length) {
+                break;
+            }
+    
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+    
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+    
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+    
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+    
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+            
+            List<List<Integer>> ret = new Solution().levelOrderBottom(root);
+            
+            String out = int2dListToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
