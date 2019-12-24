7c15639f81276dc5c1cc5b59b33ee06d8efecb4f
lin <451846939@qq.com>
2019-12-24 21:57:01 +0800

    commit

---
+++ src/main/java/leetcode101/Main2.java
@@ -0,0 +1,164 @@
+package leetcode101;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ * 说明:
+ * <p>
+ * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Main2 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if (left==null&&right==null){
+                continue;
+            }
+            boolean symmetric = symmetric(left, right);
+            if (!symmetric){
+                return false;
+            }
+            boolean s = symmetric(left.left, right.right);
+            boolean l = symmetric(left.right, right.left);
+            if (!s||!l) {
+                return false;
+            }else {
+                queue.add(left.left);
+                queue.add(right.right);
+                queue.add(left.right);
+                queue.add(right.left);
+            }
+
+        }
+        return true;
+    }
+
+    public static boolean symmetric(TreeNode left, TreeNode right) {
+        if (left == null && right != null) {
+            return false;
+        }
+        if (right == null && left != null) {
+            return false;
+        }
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left.val == right.val) {
+            return true;
+        }
+        return false;
+    }
+
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            boolean ret = isSymmetric(root);
+
+            String out = booleanToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
