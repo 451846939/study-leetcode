925cbde152f9af01f36bd9b62b6139bd75237fce
lin <451846939@qq.com>
2020-01-15 23:42:28 +0800

    commit

---
+++ src/main/java/leetcode200/Solution.java
@@ -0,0 +1,73 @@
+package leetcode200;
+
+/**
+ * 给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ *
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ *
+ * 输出: 3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-islands
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count=0;
+        if (grid==null||grid.length==0){
+            return count;
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j]=='1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] grid,int x,int y){
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == '0') {
+            return;
+        }
+        grid[x][y]='0';
+        dfs(grid, x - 1, y);
+        dfs(grid, x + 1, y);
+        dfs(grid, x, y - 1);
+        dfs(grid, x, y + 1);
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[][] grid1 = {
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}};
+        int numIslands1 = solution.numIslands(grid1);
+        System.out.println(numIslands1);
+
+        char[][] grid2 = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}};
+        int numIslands2 = solution.numIslands(grid2);
+        System.out.println(numIslands2);
+    }
+}
