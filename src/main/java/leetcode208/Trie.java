2d8a4b0da7595200b73c08aae6f9ffe80c2e6871
lin <451846939@qq.com>
2020-02-24 23:51:29 +0800

    commit

---
+++ src/main/java/leetcode208/Trie.java
@@ -0,0 +1,100 @@
+package leetcode208;
+
+/**
+ * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+ *
+ * 示例:
+ *
+ * Trie trie = new Trie();
+ *
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 true
+ * trie.search("app");     // 返回 false
+ * trie.startsWith("app"); // 返回 true
+ * trie.insert("app");
+ * trie.search("app");     // 返回 true
+ * 说明:
+ *
+ * 你可以假设所有的输入都是由小写字母 a-z 构成的。
+ * 保证所有输入均为非空字符串。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class Trie {
+
+    //    class BinaryTreeNode { char data; BinaryTreeNode left; BinaryTreeNode right; }
+    private TrieNode root = new TrieNode('/');
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Trie() {
+
+    }
+
+    /**
+     * Inserts a word into the trie.
+     */
+    public void insert(String word) {
+        TrieNode p = root;
+        char[] text = word.toCharArray();
+        for (int i = 0; i < text.length; ++i) {
+            int index = text[i] - 'a';
+            if (p.children[index] == null) {
+                TrieNode newNode = new TrieNode(text[i]);
+                p.children[index] = newNode;
+            }
+            p = p.children[index];
+        }
+        p.isEndingChar = true;
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    public boolean search(String word) {
+        TrieNode p = root;
+        char[] pattern = word.toCharArray();
+        for (int i = 0; i < pattern.length; ++i) {
+            int index = pattern[i] - 'a';
+            if (p.children[index] == null) {
+                return false; // 不存在pattern
+            }
+            p = p.children[index];
+        }
+        if (p.isEndingChar == false) {
+            // 不能完全匹配，只是前缀
+            return false;
+        } else {
+            return true; // 找到pattern
+        }
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        TrieNode p = root;
+        char[] pattern = prefix.toCharArray();
+        for (int i = 0; i < pattern.length; ++i) {
+            int index = pattern[i] - 'a';
+            if (p.children[index] == null) {
+                return false; // 不存在pattern
+            }
+            p = p.children[index];
+        }
+        return true;
+    }
+
+    public class TrieNode {
+        public char data;
+        public TrieNode[] children = new TrieNode[26];
+        public boolean isEndingChar = false;
+
+        public TrieNode(char data) {
+            this.data = data;
+        }
+    }
+}
