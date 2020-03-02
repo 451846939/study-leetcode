644557cbebb6c4407653127d0a843197a279b9e7
lin <451846939@qq.com>
2020-03-02 23:19:55 +0800

    commit

---
+++ src/main/java/leetcode206/Main.go
@@ -0,0 +1,29 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		tmp := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = tmp
+	}
+	return prev
+}
