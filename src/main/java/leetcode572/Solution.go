adc535ad30af6ddb4fae0a728b180cc857baf818
lin <451846939@qq.com>
2020-05-07 14:16:42 +0800

    commit

---
+++ src/main/java/leetcode572/Solution.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+/*
+给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+
+示例 1:
+给定的树 s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+给定的树 t：
+
+   4
+  / \
+ 1   2
+返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+
+示例 2:
+给定的树 s：
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+给定的树 t：
+
+   4
+  / \
+ 1   2
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/subtree-of-another-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+	s := TreeNode{Val: 3, Left: &TreeNode{4, &TreeNode{1, nil, nil}, &TreeNode{2, &TreeNode{0, nil, nil}, nil}}, Right: &TreeNode{5, nil, nil}}
+	t := TreeNode{4, &TreeNode{1, nil, nil}, &TreeNode{2, nil, nil}}
+	fmt.Println(isSubtree(&s, &t))
+}
+
+/**
+ * Definition for a binary tree node. */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+	if s == nil {
+		return false
+	}
+	return check(s, t) || isSubtree(s.Left, t) || isSubtree(s.Right, t)
+}
+
+func check(a, b *TreeNode) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	if a.Val == b.Val {
+		return check(a.Left, b.Left) && check(a.Right, b.Right)
+	}
+	return false
+}
