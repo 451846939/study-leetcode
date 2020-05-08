e8482c5e12aa1217d913ed21d9d5e6c085994a39
lin <451846939@qq.com>
2020-05-08 13:23:53 +0800

    commit

---
+++ src/main/java/leetcode221/Solution.go
@@ -0,0 +1,96 @@
+package main
+
+/*
+在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+
+示例:
+
+输入:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+输出: 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximal-square
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+
+}
+
+func maximalSquare(matrix [][]byte) int {
+	maxSide := 0
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return maxSide
+	}
+	rows, columns := len(matrix), len(matrix[0])
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			if matrix[i][j] == '1' {
+				maxSide = max(maxSide, 1)
+				curMaxSide := min(rows-i, columns-j)
+				for k := 1; k < curMaxSide; k++ {
+					flag := true
+					if matrix[i+k][j+k] == '0' {
+						break
+					}
+					for m := 0; m < k; m++ {
+						if matrix[i+k][j+m] == '0' || matrix[i+m][j+k] == '0' {
+							flag = false
+							break
+						}
+					}
+					if flag {
+						maxSide = max(maxSide, k+1)
+					} else {
+						break
+					}
+				}
+			}
+		}
+	}
+	return maxSide * maxSide
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+func maximalSquare2(matrix [][]byte) int {
+	dp := make([][]int, len(matrix))
+	maxSide := 0
+	for i := 0; i < len(matrix); i++ {
+		dp[i] = make([]int, len(matrix[i]))
+		for j := 0; j < len(matrix[i]); j++ {
+			dp[i][j] = int(matrix[i][j] - '0')
+			if dp[i][j] == 1 {
+				maxSide = 1
+			}
+		}
+	}
+
+	for i := 1; i < len(matrix); i++ {
+		for j := 1; j < len(matrix[i]); j++ {
+			if dp[i][j] == 1 {
+				dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+				if dp[i][j] > maxSide {
+					maxSide = dp[i][j]
+				}
+			}
+		}
+	}
+	return maxSide * maxSide
+}
