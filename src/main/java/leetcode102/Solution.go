93582f5671a22db4bcd69aea8818c083f2e6d92a
lin <451846939@qq.com>
2020-05-13 14:37:40 +0800

    commit

---
+++ src/main/java/leetcode102/Solution.go
@@ -0,0 +1,57 @@
+package main
+
+/*
+给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+
+
+
+示例：
+二叉树：[3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+
+}
+
+/**
+ * Definition for a binary tree node. */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var res [][]int
+
+func levelOrder(root *TreeNode) [][]int {
+	res = [][]int{}
+	dfs(root, 0)
+	return res
+}
+
+func dfs(root *TreeNode, level int) {
+	if root != nil {
+		if len(res) == level {
+			res = append(res, []int{})
+		}
+		res[level] = append(res[level], root.Val)
+		dfs(root.Left, level+1)
+		dfs(root.Right, level+1)
+	}
+}
