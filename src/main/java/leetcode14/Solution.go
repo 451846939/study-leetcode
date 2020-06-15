d7ea02645812372300a65fe480c27a0c40fcf0c5
lin <451846939@qq.com>
2020-06-15 14:26:42 +0800

    commit

---
+++ src/main/java/leetcode14/Solution.go
@@ -0,0 +1,58 @@
+package main
+
+/*
+
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+说明:
+
+所有输入只包含小写字母 a-z 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-common-prefix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+
+}
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	isCommonPrefix := func(length int) bool {
+		str0, count := strs[0][:length], len(strs)
+		for i := 1; i < count; i++ {
+			if strs[i][:length] != str0 {
+				return false
+			}
+		}
+		return true
+	}
+	minLength := len(strs[0])
+	for _, s := range strs {
+		if len(s) < minLength {
+			minLength = len(s)
+		}
+	}
+	low, high := 0, minLength
+	for low < high {
+		mid := (high-low+1)>>1 + low
+		if isCommonPrefix(mid) {
+			low = mid
+		} else {
+			high = mid - 1
+		}
+	}
+	return strs[0][:low]
+}
