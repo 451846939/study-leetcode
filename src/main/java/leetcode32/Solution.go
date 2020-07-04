ca493a397be3e10f45e6353a09241af473debe08
lin <451846939@qq.com>
2020-07-04 19:46:17 +0800

    commit

---
+++ src/main/java/leetcode32/Solution.go
@@ -0,0 +1,53 @@
+package main
+
+/*
+给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+
+示例 1:
+
+输入: "(()"
+输出: 2
+解释: 最长有效括号子串为 "()"
+示例 2:
+
+输入: ")()())"
+输出: 4
+解释: 最长有效括号子串为 "()()"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+
+}
+func longestValidParentheses(s string) int {
+	//dp 方法，建议使用栈
+	ans := 0
+	dp := make([]int, len(s))
+	for i := 1; i < len(s); i++ {
+		if s[i] == ')' {
+			if s[i-1] == '(' {
+				if i >= 2 {
+					dp[i] = dp[i-2] + 2
+				} else {
+					dp[i] = 2
+				}
+			} else if i-dp[i-1] > 0 && s[i-dp[i-1]-1] == '(' {
+				if i-dp[i-1] >= 2 {
+					dp[i] = dp[i-1] + dp[i-dp[i-1]-2] + 2
+				} else {
+					dp[i] = dp[i-1] + 2
+				}
+			}
+		}
+		ans = max(ans, dp[i])
+	}
+	return ans
+}
+func max(i, j int) int {
+	if i < j {
+		return j
+	}
+	return i
+}
