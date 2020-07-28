31567bba0ca2a9f793abe540b397181629596553
lin <451846939@qq.com>
2020-07-28 10:11:12 +0800

    commit

---
+++ src/main/java/leetcode104/Solution.go
@@ -0,0 +1,56 @@
+package main
+
+/*
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+func main() {
+
+}
+
+/**
+ * Definition for a binary tree node.*/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return getDepth(root, 1)
+}
+func getDepth(root *TreeNode, depth int) int {
+	if root == nil {
+		return depth - 1
+	}
+	l := getDepth(root.Left, depth+1)
+	r := getDepth(root.Right, depth+1)
+	return max(l, r)
+}
+func max(i, j int) int {
+	if i < j {
+		return j
+	}
+	return i
+}
