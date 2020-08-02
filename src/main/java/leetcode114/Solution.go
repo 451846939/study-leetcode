d927d7c6b00d15f49c923d4b22637bb9fc6371d5
archer <451846939@qq.com>
2020-08-02 12:19:06 +0800

    commit

---
+++ src/main/java/leetcode114/Solution.go
@@ -0,0 +1,60 @@
+package main
+
+/*
+给定一个二叉树，原地将它展开为一个单链表。
+
+
+
+例如，给定二叉树
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+将其展开为：
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+
+}
+
+/**
+ * Definition for a binary tree node. */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func flatten(root *TreeNode) {
+	list := preorderTraversal(root)
+	for i := 1; i < len(list); i++ {
+		prev, curr := list[i-1], list[i]
+		prev.Left, prev.Right = nil, curr
+	}
+}
+func preorderTraversal(root *TreeNode) []*TreeNode {
+	list := []*TreeNode{}
+	if root != nil {
+		list = append(list, root)
+		list = append(list, preorderTraversal(root.Left)...)
+		list = append(list, preorderTraversal(root.Right)...)
+	}
+	return list
+}
