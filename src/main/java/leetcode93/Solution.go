90c56658ba9f00371f349dbd93a72339f697ef2f
lin <451846939@qq.com>
2020-08-10 14:23:29 +0800

    commit

---
+++ src/main/java/leetcode93/Solution.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/*
+给定一个只包含数字的字符串，复原它并返回所有可能的 IP 地址格式。
+
+有效的 IP 地址正好由四个整数（每个整数位于 0 到 255 之间组成），整数之间用 '.' 分隔。
+
+
+
+示例:
+
+输入: "25525511135"
+输出: ["255.255.11.135", "255.255.111.35"]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/restore-ip-addresses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+	fmt.Println(restoreIpAddresses("25525511135"))
+}
+
+const SEG_COUNT = 4
+
+var (
+	ans      []string
+	segments []int
+)
+
+func restoreIpAddresses(s string) []string {
+	segments = make([]int, SEG_COUNT)
+	ans = []string{}
+	dfs(s, 0, 0)
+	return ans
+}
+
+func dfs(s string, segId, segStart int) {
+	// 如果找到了 4 段 IP 地址并且遍历完了字符串，那么就是一种答案
+	if segId == SEG_COUNT {
+		if segStart == len(s) {
+			ipAddr := ""
+			for i := 0; i < SEG_COUNT; i++ {
+				ipAddr += strconv.Itoa(segments[i])
+				if i != SEG_COUNT-1 {
+					ipAddr += "."
+				}
+			}
+			ans = append(ans, ipAddr)
+		}
+		return
+	}
+
+	// 如果还没有找到 4 段 IP 地址就已经遍历完了字符串，那么提前回溯
+	if segStart == len(s) {
+		return
+	}
+	// 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
+	if s[segStart] == '0' {
+		segments[segId] = 0
+		dfs(s, segId+1, segStart+1)
+	}
+	// 一般情况，枚举每一种可能性并递归
+	addr := 0
+	for segEnd := segStart; segEnd < len(s); segEnd++ {
+		addr = addr*10 + int(s[segEnd]-'0')
+		if addr > 0 && addr <= 0xFF {
+			segments[segId] = addr
+			dfs(s, segId+1, segEnd+1)
+		} else {
+			break
+		}
+	}
+}
