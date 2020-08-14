75ba0356844fbf5a928b340c35c1e53cbf074364
lin <451846939@qq.com>
2020-08-14 10:59:10 +0800

    commit

---
+++ src/main/java/leetcode20/Solution.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+/*
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+示例 1:
+
+输入: "()"
+输出: true
+示例 2:
+
+输入: "()[]{}"
+输出: true
+示例 3:
+
+输入: "(]"
+输出: false
+示例 4:
+
+输入: "([)]"
+输出: false
+示例 5:
+
+输入: "{[]}"
+输出: true
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+func main() {
+	fmt.Println(isValid("()[]{}"))
+}
+func isValid(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	if len(s) <= 1 {
+		return false
+	}
+	stack := make([]rune, 0)
+	for _, c := range []rune(s) {
+		if len(stack) <= 0 {
+			stack = append(stack, c)
+			continue
+		}
+		if stack[len(stack)-1] == '(' && c == ')' {
+			stack = stack[0 : len(stack)-1]
+			continue
+		}
+		if stack[len(stack)-1] == '[' && c == ']' {
+			stack = stack[0 : len(stack)-1]
+			continue
+		}
+		if stack[len(stack)-1] == '{' && c == '}' {
+			stack = stack[0 : len(stack)-1]
+			continue
+		}
+		stack = append(stack, c)
+	}
+	if len(stack) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
