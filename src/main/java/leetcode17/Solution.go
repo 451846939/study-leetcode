077326b444baf5a16cb1c6e4315a052f548c2878
lin <451846939@qq.com>
2020-08-26 10:53:32 +0800

    commit

---
+++ src/main/java/leetcode17/Solution.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+https://assets.leetcode-cn.com/aliyun-lc-upload/original_images/17_telephone_keypad.png
+
+示例:
+
+输入："23"
+输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+
+}
+
+var phoneMap map[string]string = map[string]string{
+	"2": "abc",
+	"3": "def",
+	"4": "ghi",
+	"5": "jkl",
+	"6": "mno",
+	"7": "pqrs",
+	"8": "tuv",
+	"9": "wxyz",
+}
+
+var combinations []string
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+	combinations = []string{}
+	backtrack(digits, 0, "")
+	return combinations
+}
+
+func backtrack(digits string, index int, combination string) {
+	if index == len(digits) {
+		combinations = append(combinations, combination)
+	} else {
+		digit := string(digits[index])
+		letters := phoneMap[digit]
+		for i := range letters {
+			backtrack(digits, index+1, combination+string(letters[i]))
+		}
+	}
+}
