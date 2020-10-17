000304de8a4aa63ebb72d538d1f515b21e6b79a4
archer <451846939@qq.com>
2020-10-17 11:07:55 +0800

    commit

---
+++ src/main/java/leetcode52/Solution.go
@@ -0,0 +1,68 @@
+package main
+
+/*
+n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+
+https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2018/10/12/8-queens.png
+
+上图为 8 皇后问题的一种解法。
+
+给定一个整数 n，返回 n 皇后不同的解决方案的数量。
+
+示例:
+
+输入: 4
+输出: 2
+解释: 4 皇后问题存在如下两个不同的解法。
+[
+ [".Q..",  // 解法 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // 解法 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+
+
+提示：
+
+皇后，是国际象棋中的棋子，意味着国王的妻子。皇后只做一件事，那就是“吃子”。当她遇见可以吃的棋子时，就迅速冲上去吃掉棋子。当然，她横、竖、斜都可走一或 N-1 步，可进可退。（引用自 百度百科 - 皇后 ）
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-queens-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+func main() {
+
+}
+func totalNQueens(n int) (ans int) {
+	columns := make([]bool, n)        // 列上是否有皇后
+	diagonals1 := make([]bool, 2*n-1) // 左上到右下是否有皇后
+	diagonals2 := make([]bool, 2*n-1) // 右上到左下是否有皇后
+	var backtrack func(int)
+	backtrack = func(row int) {
+		if row == n {
+			ans++
+			return
+		}
+		for col, hasQueen := range columns {
+			d1, d2 := row+(n-1)-col, row+col
+			if hasQueen || diagonals1[d1] || diagonals2[d2] {
+				continue
+			}
+			columns[col] = true
+			diagonals1[d1] = true
+			diagonals2[d2] = true
+			backtrack(row + 1)
+			diagonals1[d1] = false
+			diagonals2[d2] = false
+			columns[col] = false
+		}
+	}
+	backtrack(0)
+	return
+}
