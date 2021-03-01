c846a6771e8abf239e45fbb0c34fc4bfc9cf04ff
lin <451846939@qq.com>
2021-03-01 10:11:05 +0800

    commit

---
+++ src/main/java/leetcode303/Solution.rs
@@ -0,0 +1,76 @@
+/*
+给定一个整数数组 nums，求出数组从索引i到j（i≤j）范围内元素的总和，包含i、j两点。
+
+实现 NumArray 类：
+
+NumArray(int[] nums) 使用数组 nums 初始化对象
+int sumRange(int i, int j) 返回数组 nums 从索引i到j（i≤j）范围内元素的总和，包含i、j两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+
+
+示例：
+
+输入：
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+输出：
+[null, 1, -1, -3]
+
+解释：
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+
+
+提示：
+
+0 <= nums.length <= 104
+-105<= nums[i] <=105
+0 <= i <= j < nums.length
+最多调用 104 次 sumRange 方法
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/range-sum-query-immutable
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+struct NumArray {
+    pre_sum:Vec<i32>
+}
+
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl NumArray {
+
+    fn new(nums: Vec<i32>) -> Self {
+        let mut pre = 0;
+        let mut pre_sum: Vec<i32> = nums
+            .iter()
+            .map(|x| {
+                pre += x;
+                pre
+            })
+            .collect();
+
+        NumArray { pre_sum: pre_sum }
+
+    }
+
+    fn sum_range(&self, i: i32, j: i32) -> i32 {
+        if i>j {
+            return 0
+        }
+        if i==0 {
+            return self.pre_sum[j as usize]
+        }
+        self.pre_sum[j as usize]-self.pre_sum[i as usize -1]
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * let obj = NumArray::new(nums);
+ * let ret_1: i32 = obj.sum_range(i, j);
+ */
